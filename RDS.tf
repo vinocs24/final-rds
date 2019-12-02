ed5a18092d8d6b95bf72ad8acd5ee987d070c5a9
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-02 17:12:10 +0530

    Update RDS.tf

--- RDS.tf
+++ RDS.tf
@@ -14,7 +14,7 @@ resource "aws_db_instance" "wordpress" {
     name                   = var.db_name
     username               = var.db_user
     password               = var.db_password
-    availability_zone      = "us-east-1b"
+    availability_zone      = "us-west-2b"
     vpc_security_group_ids = [aws_security_group.wp-db-sg-tf.id]
     multi_az               = false
     db_subnet_group_name   = aws_db_subnet_group.default.id
