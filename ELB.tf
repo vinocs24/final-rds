a49e0e6a462975edbad7696885ef305f789805de
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-02 17:27:56 +0530

    Update ELB.tf

--- ELB.tf
+++ ELB.tf
@@ -18,7 +18,7 @@ resource "aws_elb" "default" {
         interval            = 30
     }
 
-    instances                   = aws_instance.webservers.*.id
+    instances                   = aws_instance.ec2-instance.*.id
     cross_zone_load_balancing   = true
     idle_timeout                = 100
     connection_draining         = true
