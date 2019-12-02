595e8d926147c404886b6e567eb87e4ada880a55
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-02 17:41:00 +0530

    Update installing-components.sh

--- installing-components.sh
+++ installing-components.sh
@@ -1,6 +1,26 @@
-#! /bin/bash
-sudo yum update
-sudo yum install -y httpd
-sudo chkconfig httpd on
-sudo service httpd start
-echo "<h1>Deployed via Terraform wih ELB</h1>" | sudo tee /var/www/html/index.html
+#!/bin/bash
+sudo -i
+yum update -y
+
+sudo yum install -y httpd 
+sudo amazon-linux-extras install -y php7.2 
+
+yum install wget -y
+wget http://repo.mysql.com/mysql-community-release-el6-5.noarch.rpm
+rpm -ivh mysql-community-release-el6-5.noarch.rpm
+
+yum install mysql-server -y
+yum install  php-mysqlnd -y
+
+cd /var/www/html
+echo "healthy" > healthy.html
+
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+cp -r wordpress/* /var/www/html/
+rm -rf wordpress
+rm -rf latest.tar.gz
+chmod -R 755 wp-content
+chown -R apache:apache wp-content
+chkconfig httpd on
+service httpd start
