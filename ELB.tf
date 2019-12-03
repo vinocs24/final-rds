a86b30815298836baba01a536f85a581ddcceb5f
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-03 18:45:05 +0530

    Update ELB.tf

--- ELB.tf
+++ ELB.tf
@@ -14,7 +14,7 @@ resource "aws_elb" "default" {
         healthy_threshold   = 2
         unhealthy_threshold = 2
         timeout             = 5
-        target              = "HTTP:80/healthy.html/"
+        target              = "HTTP:80/healthy.html"
         interval            = 30
     }
 
