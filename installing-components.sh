5e617006086e95a02c92b04d806fbf73f5828fdc
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-04 14:26:10 +0530

    Update installing-components.sh

--- installing-components.sh
+++ installing-components.sh
@@ -6,6 +6,7 @@ cd /var/www/html
 echo "healthy" > healthy.html
 wget https://wordpress.org/wordpress-5.1.1.tar.gz
 tar -xzf wordpress-5.1.1.tar.gz
+mv wordpress/wp-config-sample.php wordpress/wp-config.php
 cp -r wordpress/* /var/www/html/
 rm -rf wordpress
 rm -rf wordpress-5.1.1.tar.gz
